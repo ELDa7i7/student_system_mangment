d1556cb660dc884abaad44f6421c1ff0f6dc9196
ELDa7i7 <amrfathymohamed7056@gmail.com>
2022-12-16 16:12:48 +0200

    edited btw

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<style>
-   a{
-    text-decoration: none;
-    color: #3a6cf4;
-   } 
-   h1{
-    color: #3a6cf4; 
-    text-align: center;
-    padding-top:150px ;
-   }
-</style>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,12 +7,24 @@
     <title>home</title>
 </head>
 <body>
-<link rel="stylesheet" href="style.css"> 
+<link rel="stylesheet" href="style.css">          
+<header>
+    <a href="#" class="logo">Amr</a>  
+  <nav class="navigation">
+   <a href="students.php">students</a>
+    <a href="admins.php">admins</a>
+    <a href="">about us</a>
+   </nav>
+  </header> 
+  <section class="main">
    
-    <h2><a href="students.php">students</a></h2> 
-    <br> <br>
-    <h2><a href="admins.php">admins</a></h2>
-    <h1>welcome to Mr Amr fathy's website</h1>
+      <h1 class="text"><span>
+        welcome to Mr Amr fathy's website
+      </span> </h1>
+  </section>
+
+
+
   
 </body>
-</html>
+</html> 
